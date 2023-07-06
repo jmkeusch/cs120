4d800aa4a65c334f0ec0582c1ae68cbd301740f7
Jonathan Keusch <jonathankeusch@gmail.com>
2023-07-06 00:55:18 -0400

    push

--- project2/wordle.js
+++ project2/wordle.js
@@ -4,7 +4,7 @@ document.write("Hello");
 //Global Variables
 let ROUND = 0;
 let MAXROUNDS = 5;
-let WORD = "";
+let WORD = "TEST";
 let ANS = [];
 let GUESS = "";
 let pGUESS = [];
@@ -12,29 +12,25 @@ let WIN = false;
 
 
 window.onload = function() {
-    WORD = getWord();
+    //WORD = getWord();
     alert("Word is " + WORD);
 }
 
 //TODO: Call API to get word onload
-function getWord() {
-    fetch("https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase", {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-        }
-    })
-    .then(response => response.text())
-    .then(response => {
-        WORD = response;
-    })
-    return WORD;
-}
+// function getWord() {
+//     fetch("https://random-word-api.vercel.app/api?words=1&length=5&type=uppercase", {
+//         method: 'GET',
+//         headers: {
+//             'Accept': 'application/json',
+//         }
+//     })
+//     .then(response => response.text())
+//     .then(response => {
+//         WORD = response;
+//     })
+//     return WORD;
+// }
 
-//TODO: Begin Round -- use while loop
-while (WIN == false && ROUND < MAXROUNDS) {
-
-}
 
 //TODO (Optional): Verify guess before parsing
 
