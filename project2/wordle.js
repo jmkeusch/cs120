f88136b09a60f4dbb77216b9244996e3e6e2f287
Jonathan Keusch <jonathankeusch@gmail.com>
2023-07-07 11:10:41 -0400

    responsivness

--- project2/wordle.js
+++ project2/wordle.js
@@ -4,13 +4,16 @@
 let ROUND = Number(0);
 let MAXROUNDS = Number(5);
 let OBJ = new Object;
+let CHECK = new Object;
 let WORD = "";
 let ANS = [];
 let INV = [];
 let GUESS = "";
 let arrGuess = [];
+let entry = document.getElementById('input1');
+let guessBtn = document.getElementById('btn1');
+let newGameBtn = document.getElementById('btn2');
 let WIN = false;
-let newGame = false;
 
 
 //TODO: Call API to get word onload
@@ -31,6 +34,30 @@ window.onload = function getWord() {
         .catch(err => console.log(err)) 
 }
 
+function checkGuess() {
+    guess = GUESS.toLowerCase();
+    let isValid = true;
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + guess;
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+            .then((response) => response.text())
+            .then((response) => {
+                const str = JSON.stringify(response);
+                //console.log("str is " + str);
+                CHECK = JSON.parse(str);
+                //console.log("check is " + CHECK);
+            })
+            .catch(err => {
+                return false;
+            })
+    return isValid;
+}
+
 // async function assignWord() {
 //     //WORD = await getWord();
 //     console.log("In assignWord");
@@ -39,83 +66,130 @@ window.onload = function getWord() {
 //     ANS = WORD.split();
 // }
 
-if (ROUND > 5) {
-    alert("Sorry, you didn't get the word... The answer was " + WORD);
+guessBtn.onclick = () => {
+    processGuess();
 }
 
-console.log("Round is " + ROUND);
-    btnObj = document.getElementById('btn1');
-    btnObj.onclick = () => {
-        processGuess();
-    }
+newGameBtn.onclick = () => {
+    console.log("newGame Button clicked");
+    startNewGame();
+}
 
 //Submit guess on enter key
 var input = document.getElementById("input1");
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
-        document.getElementById("btn1").click();
+        guessBtn.click();
     }
 })
 
-
+//Process submitted guess word
 function processGuess() {
-   //TODO: Event handler to intake word -- Main     Function
-    //Get word value
+    ANS = WORD.split("");
+    INV = ANS;
+    console.log("INV is " + INV);
+    //Get guess value
     GUESS = $("#input1").val();
-    //Ensure 5 chars
+
+    //VALIDATION
+    //Ensure guess is 5 chars
     if (GUESS.length != 5) {
         alert("You must guess a 5-letter word!");
         GUESS = "";
+        document.getElementById('form1').reset();
         return;
     }
     //check if dictionary word
-    //Upper
+    if (!checkGuess()) {
+        alert("Please enter a valid word.");
+        GUESS = "";
+        document.getElementById('form1').reset();
+        return;
+    };
+
+    //Clear input
+    input.value = "";
+
+    //Uppercase guess word
     GUESS = GUESS.toUpperCase();
     console.log("Guess is " + GUESS);
     //Parse
-    arrGuess = GUESS.split();
-    
-    //Compare letters (in array)
-    
-    //Color cells
-};
+    arrGuess = GUESS.split("");
+    console.log("arrGuess is " + arrGuess);
+    //COMPARE WORDS
 
-//TODO: Change color
-//Case: All correct
-// document.getElementById("row"+ROUND).className = "correct"
-// //Case: Letter correct
-// document.getElementById("c"+ROUND+2).className = "present";
-// //Case: Letter present
-// document.getElementById("c"+ROUND+4).className = "present";
+    //Exact word --> Game won
+    if (GUESS === WORD) {
+        document.getElementById("row"+ROUND).className = "correct";
+        for (let i = 0; i < 5; i++) {
+            cell = document.getElementById("c"+ROUND+i);
+            cell.appendChild(document.createTextNode(GUESS[i]));
+        }
+        guessBtn.disabled = true;
+        WIN = true;
+        showNewGame();
+        alert("Congratulations! You guessed the word!");
+    } else {
+        //Loop through GUESS against WORD (in respective arrays). Mark letters by match & placement.
+        arrGuess.forEach(function (item, index) {
+            cell = document.getElementById("c"+ROUND+index);
+            cell.appendChild(document.createTextNode(item));
+            //Matched letter and placement-->Mark green
+            if (item === ANS[index]) {
+                cell.className = "correct";
+              //No match-->Just add letter to board
+            } else if (item != ANS[index]) {
+                foundIndex = ANS.findIndex(function (item1) {
+                    return item1 === item;
+                }) //Matched letter, wrong placement-->Mark yellow (Green cells takes precedence)
+                if (foundIndex != (-1) && (cell.className != "correct")) {
+                    console.log("Foundindex is " + foundIndex);
+                    cell.className = "present";
+                }
+            }
+        }) 
+    }
+    //Increment round to next row and guess attempt
+    ROUND++;
+    //Game Lost
+    if (ROUND > 5 && WIN != true) {
+        guessBtn.disabled = true;
+        showNewGame();
+        alert("Sorry, you didn't get the word. The answer was " + WORD + ".");
+    }
+};
 
 //TODO (Optional): Verify guess before parsing
 
-
-
-//Event: Display New Game Button
+//Events: Toggle 'New Game' Button
 function showNewGame () {
-    let newGameBtn = document.getElementById("btn2").style.display = "block";
+    newGameBtn = document.getElementById("btn2").style.display = "block";
 };
+function hideNewGame () {
+    newGameBtn = document.getElementById("btn2").style.display = "none";
+}
 
 //Event: Execute New Game Button
 function startNewGame () {
-    button = document.getElementById("btn2");
-    button.onclick = () => {
-        //clear board words
-        //clear colors
-        //reset counter
-        //hide new game button
-        //get new word
-    }
-}
-
-function playGame() {
-    assignWord();
-    while (WIN === false && ROUND < 5) {
-        if (WIN === true) {
-            alert("Congratulations! You guessed the word!");
-            break;
-        }
-    }
+    document.location.reload();
+    // console.log("In Start new game fxn");
+    // let i, j = 0;
+    // let cell = document.getElementById("c"+i+j);
+    // console.log("On cell " + cell);
+    // for (i; i < MAXROUNDS; i++) {
+    //     console.log("row is " + i);
+    //     for (j; j < 5; j++) {
+    //         cell = document.getElementById("c"+i+j);
+    //         $("#cell").html("");
+    //         //cell.classList.remove();
+    //     }
+    // }
+    // //clear board words
+    // //clear colors
+    // //reset counter
+    // ROUND = 0;
+    // //hide new game button
+    // hideNewGame();
+    // //get new word
 }
